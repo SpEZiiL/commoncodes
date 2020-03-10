19dcf87400090076e1206773865b158a5943f324
Michael Federczuk <federczuk.michael@protonmail.com>
2020-03-10 20:05:43 +0100

    Read major versions in src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { realpathSync } from "fs";
+import { readdirSync, realpathSync } from "fs";
 
 const ROOT_DIR = realpathSync(__dirname + "/..");
 const RAW_DIR = realpathSync(ROOT_DIR + "/raw");
@@ -9,3 +9,16 @@ const DESCRIPTION_FILENAME = "description.desc";
 const EXIT_STATUS_TABLE_FILENAME = "exit_status_table";
 const FOOTNOTES_FILENAME = "footnotes.desc";
 const SEE_ALSO_FILENAME = "see_also.yaml";
+
+const majorVersions = ((): number[] => {
+	const entries = readdirSync(RAW_DIR);
+
+	const majorVersions: number[] = [];
+
+	entries.forEach((entry) => {
+		const match = entry.match(/^v([1-9][0-9]*)$/);
+		if(match !== null) majorVersions.push(Number(match[1]));
+	});
+
+	return majorVersions;
+})();
