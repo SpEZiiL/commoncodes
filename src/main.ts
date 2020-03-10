0182a4c8ae76afd591fd1de0cde361469b0a9275
Michael Federczuk <federczuk.michael@protonmail.com>
2020-03-10 20:05:00 +0100

    Add constants src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// TODO
+import { realpathSync } from "fs";
+
+const ROOT_DIR = realpathSync(__dirname + "/..");
+const RAW_DIR = realpathSync(ROOT_DIR + "/raw");
+
+const METADATA_FILENAME = "metadata.yaml";
+const NAME_FILENAME = "name.desc";
+const DESCRIPTION_FILENAME = "description.desc";
+const EXIT_STATUS_TABLE_FILENAME = "exit_status_table";
+const FOOTNOTES_FILENAME = "footnotes.desc";
+const SEE_ALSO_FILENAME = "see_also.yaml";
